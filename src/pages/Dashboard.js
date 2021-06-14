0bb6bddc9bab17c588818c86821ecaf3ac84204d
Romaric <romaric@macbook-pro-2.home>
2021-06-14 18:14:47 +0200

    Card component

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Info, Repos, User, Search, Navbar } from "../components";
 import loadingImage from "../images/preloader.gif";
 import { GithubContext } from "../context/context";
+import { MdLocalLibrary } from "react-icons/md";
+import Card from "../components/Card";
 const Dashboard = () => {
   return (
     <main>
